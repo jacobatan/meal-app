c812fec35d4bf47f1c182b9e23a52aa3fc5c31af
Jacob Tan <jacob.tan229@gmail.com>
2021-12-02 20:16:32 +1300

    Switched to TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Recipecards from "./components/Recipecards/Recipecards";
+
+const App = () => {
+  return (
+    <div>
+      <Recipecards />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Recipecards/Recipecards.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Recipecards = () => {
+  async function test() {}
+  return (
+    <div>
+      Recipe Cards!!!
+      <p>hi</p>
+    </div>
+  );
+};
+
+export default Recipecards;
+
+// https://api.spoonacular.com/mealplanner/generate?timeFrame=day
